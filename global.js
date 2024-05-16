a38f6e460a6fd053e68cfb4e31b119337868d6ab
MichalReka <michal4529@wp.pl>
2024-05-16 22:46:29 +0200

    Arrow up added

--- global.js
+++ global.js
@@ -1,6 +1,32 @@
 
 document.getElementById('footer').innerHTML = "© Zuzanna Kuszyk, "+new Date().getFullYear();
 
+document.querySelectorAll('a[href^="#nav"]').forEach((anchor) => {
+  anchor.addEventListener("click", function (e) {
+    e.preventDefault();
+
+    document.querySelector(this.getAttribute("href")).scrollIntoView({
+      behavior: "smooth",
+    });
+  });
+
+  if(window.scrollY < 30){
+    anchor.classList.add('hidden');
+  }
+  else{
+    anchor.classList.remove('hidden');
+  }
+
+  window.addEventListener('scroll', function() {
+    if(window.scrollY < 30){
+      anchor.classList.add('hidden');
+    }
+    else{
+      anchor.classList.remove('hidden');
+    }
+  });
+});
+
 function toggleMenu() {
   document.getElementById("mobile-menu").classList.toggle("hidden");
 
@@ -9,3 +35,4 @@ function toggleMenu() {
   document.getElementById("hamburger-close").classList.toggle("hidden");
   document.getElementById("hamburger-close").classList.toggle("block");
 }
+
